b684f8ca662e725d38eecdda52d5121ccd04c5e5
CherMet90 <dddfrdd@gmail.com>
2024-01-24 12:58:44 +0300

    improve kea leases processing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import requests
 import yaml
 
 from datetime import datetime
@@ -11,6 +12,7 @@ from custom_modules.error_handling import print_errors
 from custom_modules.errors import Error, NonCriticalError
 from custom_modules.pfsense import download_config
 from custom_modules.keadhcp import KeaDHCP
+from custom_modules.mac_vendor_lookup import MacVendorLookup
 
 
 class Lease:
@@ -20,16 +22,34 @@ class Lease:
         self.netbox_prefix = NetboxDevice.get_prefix_for_ip(ip_address)
         self.ip_with_prefix = f'{ip_address}/{self.netbox_prefix.prefix.split("/")[1]}'
 
-        # Определение статуса айпишника по времени отсутствия в сети
-        self.age = self.__calculate_lease_age(start_date)
-        self.status = "active" if timedelta(
-            days=self.age) < timedelta(days=365) else "deprecated"
-
+        # If start_date is None, it's a static lease which is always active.
+        self.is_static = start_date is None
+        self.age = None if self.is_static else self.__calculate_lease_age(start_date)
+        if self.is_static:
+            self.status = "active"
+        else:
+            if self.age <= 1:
+                self.status = "active"
+            elif 1 < self.age <= 30:
+                self.status = "reserved"
+            elif 30 < self.age <= 365:
+                self.status = "dhcp"
+            else:
+                self.status = "deprecated"
+        
         # Формирование description
         self.mac_address = mac_address
         self.vendor_class = vendor_class
         self.hostname = hostname
-        self.description = f'The lease started on {start_date.split(' ', 1)[1]} / {self.mac_address if self.mac_address else "unknown mac"} / {self.hostname if self.hostname else "unknown hostname"} / {self.vendor_class if self.vendor_class else "unknown vendor"}'
+        self.description = self.__generate_description(start_date)
+
+    def __generate_description(self, start_date):
+        start_str = 'Static lease' if self.is_static else f'Lease started on {start_date.split(" ", 1)[1]}'
+        mac_str = f' / {self.mac_address if self.mac_address else "unknown mac"}'
+        hostname_str = f' / {self.hostname if self.hostname else "unknown hostname"}'
+        vendor_str = f' / {self.vendor_class if self.vendor_class else mac_vendor_lookup.get_vendor_by_mac(self.mac_address)}'
+        return start_str + mac_str + hostname_str + vendor_str
+    
 
     @staticmethod
     def __calculate_lease_age(start_date):
@@ -78,7 +98,7 @@ def parse_file_with_leases(device):
             leases.append(lease)
     return leases
 
-def get_leases_by_kea_api(router):
+def get_leases_by_kea_api():
     def convert_time(timestamp):
         time_struct = time.localtime(timestamp)
         day_of_week = time_struct.tm_wday
@@ -86,24 +106,29 @@ def get_leases_by_kea_api(router):
         formatted_date = time.strftime("%Y/%m/%d %H:%M:%S", time_struct)
         final_output = f'{day_of_week} {formatted_date}'
         return final_output
-    
-    services = NetboxDevice.get_services_by_vm(router)
-    for service in services:
-        if service.description == 'Kea DHCP API':
-            api_port = service.ports[0]
-            break
-    if not api_port:
-        logger.error('Kea DHCP API port not found')
-        return None
-    
-    KeaDHCP.set_kea_agent_address(router.primary_ip.address.split('/')[0], api_port)
-    logger.info(f'Connection to Kea-agent {KeaDHCP.url} established')
-    leases = KeaDHCP.lease_get_all()
+
+    # Получение лизов
+    granted_leases = keadhcp.lease_get_all()
+    static_leases = keadhcp.static_get_all()
+    combined_list = granted_leases + static_leases
+    # Удаление дубликатов
+    seen = set()
+    leases = []
+    for item in combined_list:
+        ip = item['ip-address']
+        if ip not in seen:
+            seen.add(ip)
+            leases.append(item)
     logger.debug(f'{len(leases)} leases received')
     
     processed_leases = []
     for index, lease in enumerate(leases):
-        processed_lease = Lease(lease['ip-address'], convert_time(lease['cltt'] - lease['valid-lft']), lease['hw-address'], None, lease['hostname'])
+        try:
+            cltt_time = convert_time(lease['cltt']) if 'cltt' in lease else None
+        except Exception as e:
+            logger.error(f'Error converting cltt for lease: {e}')
+            cltt_time = None
+        processed_lease = Lease(lease['ip-address'], cltt_time, lease['hw-address'], None, lease['hostname'])
         processed_leases.append(processed_lease)
         logger.debug(f"Processed lease count: {index + 1}")
 
@@ -112,22 +137,42 @@ def get_leases_by_kea_api(router):
 def process_leases(leases):
     for lease in leases:
         try:
-            logger.debug(f'{lease.ip_address} handling...')
             NetboxDevice.create_ip_address(
                 lease.ip_address, lease.ip_with_prefix, status=lease.status, description=lease.description)
         except Error:
             continue
 
-
+def connect_to_kea_agent():
+    services = NetboxDevice.get_services_by_vm(router)
+    for service in services:
+        if service.description == 'Kea DHCP API':
+            api_port = service.ports[0]
+            break
+    if not api_port:
+        logger.error('Kea DHCP API port not found')
+        return None
+    return KeaDHCP(router.primary_ip.address.split('/')[0], api_port)
+    
+def clear_leases():
+    pools = keadhcp.get_pools()
+    for pool in pools:
+        start_ip, end_ip = pool.split('-')
+        NetboxDevice.remove_ip_range(start_ip, end_ip)
+        logger.info(f'{start_ip} - {end_ip} cleared')
+    logger.debug(f'{len(pools)} pools found')
+    
 # Загрузка данных из файла настроек
 with open('settings.yaml', 'r') as file:
     settings_data = yaml.safe_load(file)
 router_settings = settings_data.get('router_settings', {})
 routers_to_skip = router_settings.get('skip_routers', [])
-kea_dhcp = router_settings.get('kea_dhcp', [])
+kea_dhcps = router_settings.get('kea_dhcp', [])
 
 # Загрузка переменных окружения из .env
 load_dotenv(dotenv_path='.env')
+# Загрузка кэша мак-адресов
+mac_vendor_lookup = MacVendorLookup()
+# Подключение к Netbox
 NetboxDevice.create_connection()
 NetboxDevice.get_roles()
 router_devices = NetboxDevice.get_vms_by_role(
@@ -136,10 +181,13 @@ for router in router_devices:
     # Skip routers from settings
     if router['name'] in routers_to_skip:
         continue
-    if router['name'] in kea_dhcp:
-        leases = get_leases_by_kea_api(router)
+    if router['name'] in kea_dhcps:
+        keadhcp = connect_to_kea_agent()
+        leases = get_leases_by_kea_api()
+        clear_leases()
     else:
         leases = parse_file_with_leases(router)
     # input(f'{len(leases)} leases found. Press Enter to process {router.name}...')   # for debug only
     process_leases(leases)
+mac_vendor_lookup.save_cache()  # Save mac vendor cache
 print_errors()
